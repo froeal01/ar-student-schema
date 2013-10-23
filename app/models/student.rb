100948459b581be2b222e5aa6b9c3720bcbe4c6b
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-23 17:59:15 -0500

    .

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,23 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
+  validates_inclusion_of :age, in: 6..99
+  validates_format_of :phone, with: /(\D*\d){10}/
+
+  def name
+    "#{first_name} #{last_name}"
+   end
+
+  def age
+   present = DateTime.now
+   ((present - self.birthday)/365.25).to_i
+  end
+
 end
+
+
+    # v=Student.new(:first_name => 'Alex', :last_name => 'Froelich', :gender => 'M', :birthday => DateTime.new(1985,8,1))
+    # p v.age
+    # p v.name
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,13 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name, :last_name, :gender, :email, :phone
+      t.date :birthday
+      t.timestamps :created_at, :updated_at
+    end
   end
 end
